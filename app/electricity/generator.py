a11737b2a98c81f4aa3c10444f2935c0e1b0f027
Flavio Mello <flaviorafmello@gmail.com>
2019-07-21 19:32:23 +0100

    Create Generator

---
+++ app/electricity/generator.py
@@ -0,0 +1,10 @@
+class generator:
+
+    def __init__(self, initialOutput):
+        self.output = initialOutput # output => Pg
+
+    def setOutput(self, newOutput):
+        self.output = newOutput
+
+    def getOutput(self):
+        return self.output
