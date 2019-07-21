ba40f132d512f3a9541530dda035ebefba943f51
Flavio Mello <flaviorafmello@gmail.com>
2019-07-21 18:38:16 +0100

    Create folder structure and Load class

---
+++ app/electricity/load.py
@@ -0,0 +1,9 @@
+class Load:
+    def __init__(self, initialLoad):
+        self.load = initialLoad # Load => Pl
+
+    def setLoad(self, newLoad):
+        self.load = newLoad
+
+    def getLoad(self):
+        return self.load
