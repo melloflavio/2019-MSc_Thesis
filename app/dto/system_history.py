aebe630e0dd37aaff526d718be1188a2f614dd06
Flavio Mello <flaviorafmello@gmail.com>
2019-07-29 16:55:01 +0100

    Move History to standalone singleton instead of a field within ElectricalSystem

--- app/dto/system_history.py
+++ app/dto/system_history.py
@@ -1,7 +1,9 @@
 from typing import Dict, List, NamedTuple
+from singleton_decorator import singleton
 
+@singleton
 class SystemHistory(NamedTuple):
-  totalPower: List[float]
-  frequency: List[float]
-  generators: Dict[str, List[float]]
-  loads: Dict[str, List[float]]
+  totalPower: List[float] = []
+  frequency: List[float] = []
+  generators: Dict[str, List[float]] = {}
+  loads: Dict[str, List[float]] = {}
--- app/electricity/electrical_system.py
+++ app/electricity/electrical_system.py
@@ -9,12 +9,3 @@ class ElectricalSystem:
     self.areaDynamics = AreaDynamics(electricalConstants)
     self.loads = loads
     self.generators = generators
-    self.history = SystemHistory(
-      totalPower = [],
-      frequency =  [],
-      generators = { gen.getId(): [gen.getOutput()] for gen in self.generators },
-      loads = { load.getId(): [load.getLoad()] for load in self.loads },
-    )
-
-  def getHistory(self):
-    return self.history
