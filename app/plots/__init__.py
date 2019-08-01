a70272ef6b895e2ebba97695dc169d394aa48567
Flavio Mello <flaviorafmello@gmail.com>
2019-08-01 16:27:24 +0100

    Plot historic frequency data

--- app/plots/__init__.py
+++ app/plots/__init__.py
@@ -1,1 +1,2 @@
 from .color_palette import COLOR_PALETTE
+from .frequency_plot import plot_frequency
---
+++ app/plots/frequency_plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+from dto import ElectricalConstants, SystemHistory
+
+from .color_palette import COLOR_PALETTE
+
+def plot_frequency(figure_num=0):
+
+  # Get series to be plotted
+  frequencySeries = SystemHistory().frequency
+  nSteps = len(frequencySeries)
+  stepsSeries = range(nSteps)
+  nominalFrequency = ElectricalConstants().nominalFrequency
+  nominalSeries = [nominalFrequency] * nSteps
+
+  plt.figure(figure_num)
+
+  # Declare colors to be used
+  colorNominalFreq = COLOR_PALETTE[0]
+  colorObservedFreq = COLOR_PALETTE[1]
+
+  # Plot data
+  plt.plot(stepsSeries, nominalSeries, color=colorNominalFreq)
+  plt.plot(stepsSeries, frequencySeries, color=colorObservedFreq)
+  plt.legend(['Nominal', 'Observed'])
+
+  plt.xlabel('Steps', fontsize=12)
+  plt.ylabel('System Frequency (Hz)', fontsize=12)
+
+  plt.title('System Frequency (Hz) x Time (Steps)', fontsize=14)
+
+  plt.show()
