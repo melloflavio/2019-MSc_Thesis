1e5d99aa2275517b43be15b0c689df350e4ca2bc
Flavio Mello <flaviorafmello@gmail.com>
2019-08-05 18:35:18 +0100

    Change scope of variables to allow for more clarity in maddpg critic

--- app/learning/maddpg_critic.py
+++ app/learning/maddpg_critic.py
@@ -1,59 +1,63 @@
 import tensorflow as tf
 
-class critic_maddpg():
-  """ Critic network that estimates the policy of the maddpg algorithm"""
-  def __init__(self,h_size,cell,sc,num_variables):
+class criticMaddpg():
+  """ Critic network that estimates the value of the maddpg algorithm"""
+  def __init__(self, hSize, cell, scope, numVariables):
 
     # Define the model (input-hidden layers-output)
-    self.s = tf.placeholder(shape=[None,1],dtype=tf.float32)
-    self.a = tf.placeholder(shape=[None,1],dtype=tf.float32)
-    self.a_o = tf.placeholder(shape=[None,1],dtype=tf.float32)
-    self.inputs = tf.concat([self.s,self.a,self.a_o],axis=1)
-    self.initializer = tf.contrib.layers.xavier_initializer()
+    self.s = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    self.a = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    self.a_o = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+    self.inputs = tf.concat([self.s, self.a, self.a_o], axis=1)
+    initializer = tf.contrib.layers.xavier_initializer()
 
     # LSTM to encode temporal information
-    self.batch_size = tf.placeholder(dtype=tf.int32,shape=[])   # batch size
+    self.batchSize = tf.placeholder(dtype=tf.int32, shape=[])   # batch size
     self.trainLength = tf.placeholder(dtype=tf.int32)           # trace lentgth
-    self.rnnInp = tf.reshape(self.inputs,[self.batch_size,self.trainLength,3])
+    rnnInput = tf.reshape(self.inputs, [self.batchSize, self.trainLength, 3])
 
-    self.state_in = cell.zero_state(self.batch_size,tf.float32)
-    self.rnn,self.rnn_state = tf.nn.dynamic_rnn(inputs=self.rnnInp,cell=cell,
-                              dtype=tf.float32,initial_state=self.state_in,scope=sc+'_rnn')
-    self.rnn = tf.reshape(self.rnn,shape=[-1,h_size])
+    self.stateIn = cell.zero_state(self.batchSize, tf.float32)
+    rnn, self.rnnState = tf.nn.dynamic_rnn(inputs=rnnInput, cell=cell,
+                              dtype=tf.float32, initial_state=self.stateIn, scope=scope+'_rnn')
+    rnn = tf.reshape(rnn, shape=[-1, hSize])
 
     # MLP on top of LSTM
-    self.b1 = tf.Variable(self.initializer([1,1000]))
-    self.W1 = tf.Variable(self.initializer([h_size,1000]))
-    self.h1 = tf.nn.relu(tf.matmul(self.rnn,self.W1)+self.b1)
+    l1_bias = tf.Variable(initializer([1, 1000]))
+    l1_weights = tf.Variable(initializer([hSize, 1000]))
+    layer_1 = tf.nn.relu(tf.matmul(rnn, l1_weights)+l1_bias)
 
-    self.b2 = tf.Variable(self.initializer([1,100]))
-    self.W2 = tf.Variable(self.initializer([1000,100]))
-    self.h2 = tf.nn.relu(tf.matmul(self.h1,self.W2)+self.b2)
+    l2_bias = tf.Variable(initializer([1, 100]))
+    l2_weights = tf.Variable(initializer([1000, 100]))
+    layer_2 = tf.nn.relu(tf.matmul(layer_1, l2_weights)+l2_bias)
 
-    self.b3 = tf.Variable(self.initializer([1,50]))
-    self.W3 = tf.Variable(self.initializer([100,50]))
-    self.h3 = tf.nn.relu(tf.matmul(self.h2,self.W3)+self.b3)
+    l3_bias = tf.Variable(initializer([1, 50]))
+    l3_weights = tf.Variable(initializer([100, 50]))
+    layer_3 = tf.nn.relu(tf.matmul(layer_2, l3_weights)+l3_bias)
 
-    self.b4 = tf.Variable(self.initializer([1,1]))
-    self.W4 = tf.Variable(self.initializer([50,1]))
-    self.Q = tf.matmul(self.h3,self.W4)+self.b4
+    l4_bias = tf.Variable(initializer([1, 1]))
+    l4_weights = tf.Variable(initializer([50, 1]))
+    self.Q = tf.matmul(layer_3, l4_weights)+l4_bias # Critic output is the estimated Q value
 
     # Take params of the main actor network
-    self.network_params = tf.trainable_variables()[num_variables:]
+    self.networkParams = tf.trainable_variables()[numVariables:]
 
     # Obtained from the target network (double architecture)
-    self.target_Q = tf.placeholder(tf.float32, [None, 1])
+    self.targetQ = tf.placeholder(tf.float32,  [None,  1])
 
     # Loss function and optimization of the critic
-    self.loss = tf.reduce_mean(tf.square(self.target_Q-self.Q))
-    self.optimizer = tf.train.AdamOptimizer(1e-4)
-    self.upd = self.optimizer.minimize(self.loss)
+    lossFn = tf.reduce_mean(tf.square(self.targetQ-self.Q))
+    optimizer = tf.train.AdamOptimizer(1e-4)
+    self.upd = optimizer.minimize(lossFn)
 
     # Get the gradient for the actor
-    self.critic_gradients = tf.gradients(self.Q,self.a)
+    self.critic_gradients = tf.gradients(self.Q, self.a)
 
-  def createOpHolder(self,params,tau):
+  def createOpHolder(self, params, tau):
     """ Use target network op holder if needed"""
-    self.update_network_params = [self.network_params[i].assign(tf.multiply(params[i], tau) +
-                                  tf.multiply(self.network_params[i], 1. - tau))
-                                  for i in range(len(self.network_params))]
+    networkParamSize = len(self.networkParams)
+    self.updateNetworkParams = [None]*networkParamSize
+
+    for i in range(networkParamSize):
+      assignAction = self.networkParams[i].assign(
+          tf.multiply(params[i],  tau) + tf.multiply(self.networkParams[i],  1. - tau))
+      self.updateNetworkParams[i] = assignAction
