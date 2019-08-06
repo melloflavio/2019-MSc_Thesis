77c75da0726ac69616dde1f552f4526e9256fd90
Flavio Mello <flaviorafmello@gmail.com>
2019-08-06 16:26:28 +0100

    Modify learning params to declare the shape of the NN used

--- app/learning/learning_params.py
+++ app/learning/learning_params.py
@@ -1,5 +1,12 @@
-from typing import NamedTuple
+from typing import NamedTuple, Dict
 from singleton_decorator import singleton
+
+class NeuralNetworkShape(NamedTuple):
+  layer_00_ltsm:   int # Initial LTSM layer
+  layer_01_mlp_01: int # MLP Layer 1
+  layer_02_mlp_02: int # MLP Layer 2
+  layer_03_mlp_03: int # MLP Layer 3
+  layer_04_mlp_04: int # MLP Layer 4 (output layer)
 
 @singleton
 class LearningParams(NamedTuple):
@@ -10,3 +17,10 @@ class LearningParams(NamedTuple):
   steps: int      = 100   # Number of steps per learning episode
   traceSize: int  = 8     # Number of steps each sampled episode should contain
   batchSize: int  = 4     # Number of episodes sampled from experience buffer
+  nnShape = NeuralNetworkShape(
+      layer_00_ltsm = 100,    # Initial LTSM layer
+      layer_01_mlp_01 = 1000, # MLP Layer 1
+      layer_02_mlp_02 = 100,  # MLP Layer 2
+      layer_03_mlp_03 = 50,   # MLP Layer 3
+      layer_04_mlp_04 = 50,   # MLP Layer 4 (output layer)
+  )
