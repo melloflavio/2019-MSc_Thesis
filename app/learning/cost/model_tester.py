0c19b020c3448feb0a7ff0cbc4d80de028cbbee6
Flavio Mello <flaviorafmello@gmail.com>
2019-09-01 19:34:40 +0100

    Copy all learning files to separate Cost folder

---
+++ app/learning/cost/model_tester.py
@@ -0,0 +1,48 @@
+from typing import List
+import tensorflow as tf
+
+from electricity import ElectricalSystemFactory
+from dto import ElectricalSystemSpecs, NodePowerUpdate
+from models import getPathForModel
+
+from .learning_agent import Agent
+
+class ModelTester():
+  @staticmethod
+  def testAgents(electricalSystemSpecs: ElectricalSystemSpecs, modelName: str, stepsToTest: int = 500):
+    # Clear existing graph
+    tf.compat.v1.reset_default_graph()
+
+    # Recreate the testing environment/TF variable placeholders
+    elecSystem = ElectricalSystemFactory.create(electricalSystemSpecs)
+    allAgents: List[Agent] = [Agent(generator.id_) for generator in electricalSystemSpecs.generators]
+
+    allRewards = [] # Used to plot reward history
+
+    tfSaver = tf.compat.v1.train.Saver() # Saver obj used to restore session
+    modelPath = getPathForModel(modelName)
+
+    # Main TF loop
+    with tf.compat.v1.Session() as tfSession:
+      tfSaver.restore(tfSession, modelPath)
+
+      # Test for 1000 steps
+      for stepIdx in range(stepsToTest):
+        # Get all agents' actions
+        deltaFreqOriginal = elecSystem.getCurrentDeltaF()
+        allActions = [agent.runActorAction(tfSession, deltaFreqOriginal) for agent in allAgents]
+        allActions = [action[0, 0] for action in allActions]
+
+        # Execute agents'actions (i.e. update the generators' power output)
+        agentIds = [agent.getId() for agent in allAgents]
+        generatorUpdates = [NodePowerUpdate(
+            id_=agentId,
+            deltaPower=action
+          ) for (agentId, action) in zip(agentIds, allActions)]
+        elecSystem.updateGenerators(generatorUpdates)
+
+        deltaFreqNew = elecSystem.getCurrentDeltaF()
+        earnedReward = 2**(10-abs(deltaFreqNew)) # TODO Calculate reward according to a given strategy
+        allRewards.append(earnedReward)
+
+    return elecSystem, allRewards
