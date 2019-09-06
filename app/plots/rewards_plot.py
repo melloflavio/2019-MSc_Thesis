703109f0296e562dac1a69d123d369e99598cca1
Flavio Mello <flaviorafmello@gmail.com>
2019-09-06 17:52:32 +0100

    Fix rewards plot to unravel dicts inside plotRewardComponents

--- app/plots/rewards_plot.py
+++ app/plots/rewards_plot.py
@@ -5,9 +5,15 @@ import pandas as pd
 
 from .plot_constants import COLOR_PALETTE, FIG_SIZE, FONT_SIZES
 
-def plotRewardComponents(rewardComponents: Dict[str, List[float]], figureNum=0):
+def plotRewardComponents(rewardDictList: List[Dict[str, float]], figureNum=0):
+  # details list is a collection of dicts. Needs to be consolidated into a dict of lists
+  df = pd.DataFrame(rewardDictList)
+  rewardComponents = df.to_dict(orient='list')
+
+  # New plot figure
   plt.figure(figureNum, figsize=FIG_SIZE)
 
+  # Plot each line
   for index, (componentName, valueList) in enumerate(rewardComponents.items()):
     steps = range(len(valueList))
     plotColor = COLOR_PALETTE[index%len(COLOR_PALETTE)]
@@ -24,8 +30,4 @@ def plotRewardComponents(rewardComponents: Dict[str, List[float]], figureNum=0):
 def plotExperimentRewardProgression(allRewards):
 
   for idx, rewardDictList in enumerate(allRewards):
-    # details list is a collection of dicts. Needs to be consolidated into a dict of lists
-    df = pd.DataFrame(rewardDictList)
-    rewardList = df.to_dict(orient='list')
-
-    plotRewardComponents(rewardComponents=rewardList, figureNum=idx)
+    plotRewardComponents(rewardDictList=rewardDictList, figureNum=idx)
