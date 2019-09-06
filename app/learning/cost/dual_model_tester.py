ae71d7fc94add20aa1b89cfa601ff8a63e17fa4a
Flavio Mello <flaviorafmello@gmail.com>
2019-09-06 17:45:21 +0100

    Change dual model tester instance instead of static only

--- app/learning/cost/dual_model_tester.py
+++ app/learning/cost/dual_model_tester.py
@@ -12,58 +12,55 @@ from ..learning_agent import Agent
 FREQUENCY = 'frequency'
 COST = 'cost'
 
-_tfSessions = {
-    FREQUENCY: None,
-    COST: None,
-  }
-
-_allAgents = {
-  FREQUENCY: None,
-  COST: None,
-}
-
-_allRewards = {
-  FREQUENCY: [],
-  COST: [],
-  'cost_components': []
-}
-
 class DualModelTester():
+  def __init__(self):
+    self._tfSessions = {
+        FREQUENCY: None,
+        COST: None,
+    }
 
-  @staticmethod
-  def loadTfModels(modelNameFreq: str, modelNameCost: str, generatorIds):
+    self._allAgents = {
+        FREQUENCY: None,
+        COST: None,
+    }
+
+    self._allRewards = {
+        FREQUENCY: [],
+        COST: [],
+        'cost_components': []
+    }
+
+  def loadTfModels(self, modelNameFreq: str, modelNameCost: str, generatorIds):
     # Declare both models TF graphs and sessions
     tfGraphs = {
       FREQUENCY: tf.Graph(),
       COST: tf.Graph(),
     }
-    _tfSessions[FREQUENCY] = tf.compat.v1.Session(graph=tfGraphs[FREQUENCY])
-    _tfSessions[COST] = tf.compat.v1.Session(graph=tfGraphs[COST])
+    self._tfSessions[FREQUENCY] = tf.compat.v1.Session(graph=tfGraphs[FREQUENCY])
+    self._tfSessions[COST] = tf.compat.v1.Session(graph=tfGraphs[COST])
 
     tfSavers = {}
     # Recreate agents within each graph
     with tfGraphs[FREQUENCY].as_default():
-      _allAgents[FREQUENCY]: List[Agent] = [Agent(id_) for id_ in generatorIds]
+      self._allAgents[FREQUENCY]: List[Agent] = [Agent(id_) for id_ in generatorIds]
       tfSavers[FREQUENCY] = tf.compat.v1.train.Saver()
     with tfGraphs[COST].as_default():
-      _allAgents[COST]: List[CostAgent] = [CostAgent(id_) for id_ in generatorIds]
+      self._allAgents[COST]: List[CostAgent] = [CostAgent(id_) for id_ in generatorIds]
       tfSavers[COST] = tf.compat.v1.train.Saver()
 
     # Restore TF sessions
 
     modelPathFreq = getPathForModel(modelNameFreq)
-    tfSavers[FREQUENCY].restore(_tfSessions[FREQUENCY], modelPathFreq)
+    tfSavers[FREQUENCY].restore(self._tfSessions[FREQUENCY], modelPathFreq)
     modelPathCost = getPathForModel(modelNameCost)
-    tfSavers[COST].restore(_tfSessions[COST], modelPathCost)
+    tfSavers[COST].restore(self._tfSessions[COST], modelPathCost)
 
-  @staticmethod
-  def testAgents(electricalSystemSpecs: ElectricalSystemSpecs, modelNameFreq: str, modelNameCost: str, rewardFnCost=costRewardFunction, stepsToTest: int = 500, frequencyWeight=0.7):
+  def testAgents(self, electricalSystemSpecs: ElectricalSystemSpecs, modelNameFreq: str, modelNameCost: str, rewardFnCost=costRewardFunction, stepsToTest: int = 500, frequencyWeight=0.7):
 
     # Recreate simulation environment
     elecSystem = ElectricalSystemFactory.create(electricalSystemSpecs)
-    _initTotalZ = sum(elecSystem.getGeneratorsOutputs().values())
 
-    DualModelTester.loadTfModels(
+    self.loadTfModels(
       modelNameFreq=modelNameFreq,
       modelNameCost=modelNameCost,
       generatorIds=[generator.id_ for generator in electricalSystemSpecs.generators],
@@ -74,15 +71,15 @@ class DualModelTester():
       generatorsOutputsOrigin = elecSystem.getGeneratorsOutputs()
       totalOutputOrigin = sum(generatorsOutputsOrigin.values())
 
-      allActionsFreq = DualModelTester.getActionsFreq(elecSystem)
-      allActionsCost = DualModelTester.getActionsCost(elecSystem)
+      allActionsFreq = self.getActionsFreq(elecSystem)
+      allActionsCost = self.getActionsCost(elecSystem)
 
       costWeight = 1 - frequencyWeight
 
       actionsCombined = [frequencyWeight*actionFreq + costWeight*actionCost for (actionFreq, actionCost) in zip(allActionsFreq, allActionsCost)]
 
       # Execute agents'actions (i.e. update the generators' power output)
-      agentIds = [agent.getId() for agent in _allAgents[FREQUENCY]] # Both agent collections have the same ids...
+      agentIds = [agent.getId() for agent in self._allAgents[FREQUENCY]] # Both agent collections have the same ids...
       generatorUpdates = [NodePowerUpdate(
           id_=agentId,
           deltaPower=action
@@ -92,38 +89,36 @@ class DualModelTester():
       # Calculate earned rewards per objective
       # Frequency
       deltaFreqDestination = elecSystem.getCurrentDeltaF()
-      freqReward=2**(10-abs(deltaFreqDestination))
-      _allRewards[FREQUENCY].append(freqReward) # TODO Calculate reward according to a given strategy
+      freqReward = 2**(10-abs(deltaFreqDestination))
+      self._allRewards[FREQUENCY].append(freqReward) # TODO Calculate reward according to a given strategy
 
       # Cost
       generatorsOutputsDestination = elecSystem.getGeneratorsOutputs()
       totalOutputDestination = sum(generatorsOutputsDestination.values())
       totalCost = elecSystem.getTotalCost()
-      rewardCost, rewardComponentsCost = rewardFnCost(totalOutputTarget=totalOutputOrigin,  totalOutputDestination=totalOutputDestination, totalCost=totalCost)
-      _allRewards[COST].append(rewardCost)
-      _allRewards['cost_components'].append(rewardComponentsCost)
+      rewardCost, rewardComponentsCost = rewardFnCost(totalOutputTarget=totalOutputOrigin, totalOutputDestination=totalOutputDestination, totalCost=totalCost)
+      self._allRewards[COST].append(rewardCost)
+      self._allRewards['cost_components'].append(rewardComponentsCost)
 
     # Close tf sessions after using them
-    for tfSession in _tfSessions.values():
+    for tfSession in self._tfSessions.values():
       tfSession.close()
 
-    return elecSystem, _allRewards
+    return elecSystem, self._allRewards
 
-  @staticmethod
-  def getActionsCost(elecSystem):
+  def getActionsCost(self, elecSystem):
     generatorsOutputsOrigin = elecSystem.getGeneratorsOutputs()
     totalOutput = sum(generatorsOutputsOrigin.values())
 
     allStatesOrigin = {actorId: {'genOutput': output, 'totalOutput':totalOutput} for actorId, output in generatorsOutputsOrigin.items()}
-    actionsCost = [agent.runActorAction(_tfSessions[COST], allStatesOrigin.get(agent.getId())) for agent in _allAgents[COST]]
+    actionsCost = [agent.runActorAction(self._tfSessions[COST], allStatesOrigin.get(agent.getId())) for agent in self._allAgents[COST]]
     actionsCost = [action[0, 0] for action in actionsCost]
 
     return actionsCost
 
-  @staticmethod
-  def getActionsFreq(elecSystem):
+  def getActionsFreq(self, elecSystem):
     deltaFreqOriginal = elecSystem.getCurrentDeltaF()
-    actionsFreq = [agent.runActorAction(_tfSessions[FREQUENCY], deltaFreqOriginal) for agent in _allAgents[FREQUENCY]]
+    actionsFreq = [agent.runActorAction(self._tfSessions[FREQUENCY], deltaFreqOriginal) for agent in self._allAgents[FREQUENCY]]
     actionsFreq = [action[0, 0] for action in actionsFreq]
 
     return actionsFreq
