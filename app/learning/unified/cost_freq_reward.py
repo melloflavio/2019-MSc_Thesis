108f01fa2c4c560ac0be1797046fb2e8d4fc24f2
Flavio Mello <flaviorafmello@gmail.com>
2019-09-14 13:17:12 +0100

    Copy cost_freq models to serve as base for the unified

---
+++ app/learning/unified/cost_freq_reward.py
@@ -0,0 +1,7 @@
+def costRewardFunction(deltaFreq, totalCost):
+    totalCost = totalCost/(10000.0) # Scale down cost to levels near the ones found in output differential (e.g. 10 */ 10)
+    costComponent = 2**(-1*(totalCost**2)/50)
+    freqComponent = 2**(-1*(deltaFreq**2)/2)
+    earnedReward = 10*costComponent*freqComponent
+
+    return earnedReward, {'cost': costComponent, 'freq': freqComponent, 'total':earnedReward}
