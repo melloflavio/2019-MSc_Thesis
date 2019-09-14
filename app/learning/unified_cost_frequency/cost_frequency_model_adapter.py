c1e84480a3789a5a540df29a04280f4c86c3bb6b
Flavio Mello <flaviorafmello@gmail.com>
2019-09-14 17:29:26 +0100

    Set all modelAdapter constants as @property to allow abstract class enforcing

--- app/learning/unified_cost_frequency/cost_frequency_model_adapter.py
+++ app/learning/unified_cost_frequency/cost_frequency_model_adapter.py
@@ -1,32 +1,31 @@
 from .cost_frequency_actor import ActorCostFrequency
 from .cost_frequency_critic import CriticCostFrequency
 
-# def defaultRewardFunction(deltaFreq, totalCost):
-#     totalCost = totalCost/(10000.0) # Scale down cost to levels near the ones found in output differential (e.g. 10 */ 10)
-#     costComponent = 2**(-1*(totalCost**2)/50)
-#     freqComponent = 2**(-1*(deltaFreq**2)/2)
-#     earnedReward = 10*costComponent*freqComponent
+class ModelAdapterCostFrequency():
 
-#     return earnedReward, {'cost': costComponent, 'freq': freqComponent, 'total':earnedReward}
+  def __init__(self, rewardFn=None):
+    self._rewardFn = rewardFn if rewardFn is not None else self._defaultRewardFunction.__func__
 
-def _defaultRewardFunction(deltaFreq, totalCost):
+  @property
+  def SCOPE_PREFIX(self):
+    return 'cost_freq'
+
+  @property
+  def Actor(self):
+    return ActorCostFrequency
+
+  @property
+  def Critic(self):
+    return CriticCostFrequency
+
+  @classmethod
+  def _defaultRewardFunction(deltaFreq, totalCost):
     totalCost = totalCost/(10000.0) # Scale down cost to levels near the ones found in output differential (e.g. 10 */ 10)
     costComponent = 2**(-1*(totalCost**2)/50)
     freqComponent = 2**(-1*(deltaFreq**2)/2)
     earnedReward = 10*costComponent*freqComponent
 
     return earnedReward, {'cost': costComponent, 'freq': freqComponent, 'total':earnedReward}
-
-
-class ModelAdapterCostFrequency():
-
-  SCOPE_PREFIX = 'cost'
-
-  Actor = ActorCostFrequency
-  Critic = CriticCostFrequency
-
-  def __init__(self, rewardFn=_defaultRewardFunction):
-    self._rewardFn = rewardFn
 
   def shouldStopEarly(self, elecSystem):
     costDifferential = elecSystem.getCostOptimalDiferential()
@@ -35,11 +34,15 @@ class ModelAdapterCostFrequency():
 
   ## TODO enforce typing
   def observeStates(self, elecSystem):
-    deltaFreqOriginal = elecSystem.getCurrentDeltaF()
-    generatorsOutputsOrigin = elecSystem.getGeneratorsOutputs()
-    totalOutputOrigin = sum(generatorsOutputsOrigin.values())
-    allStatesOrigin = {actorId: {'genOutput': output, 'totalOutput':totalOutputOrigin, 'deltaFreq':deltaFreqOriginal} for actorId, output in generatorsOutputsOrigin.items()}
-    return allStatesOrigin
+    deltaFreq = elecSystem.getCurrentDeltaF()
+    generatorsOutputs = elecSystem.getGeneratorsOutputs()
+    totalOutput = sum(generatorsOutputs.values())
+    allStates = {actorId: {
+      'genOutput': output,
+      'totalOutput':totalOutput,
+      'deltaFreq':deltaFreq
+      } for actorId, output in generatorsOutputs.items()}
+    return allStates
 
   def calculateReward(self, elecSystem):
     deltaFreq = elecSystem.getCurrentDeltaF()
