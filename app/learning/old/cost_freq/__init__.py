a27e16f7635ae6a78519ec1a1074e8c62f65561e
Flavio Mello <flaviorafmello@gmail.com>
2019-09-14 23:14:39 +0100

    Cleanup old learning implementations

--- app/learning/old/cost_freq/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cost_freq_actor import CostActorMaddpg
-from .cost_freq_critic import CostCriticMaddpg
-from .cost_freq_learning_agent import CostAgent
-from .cost_freq_model_tester import CostModelTester
-from .cost_freq_model_trainer import CostModelTrainer
