4166e7a7b8a57a20bfad1805deb98bfdb5adb8ca
Flavio Mello <flaviorafmello@gmail.com>
2019-09-15 23:40:44 +0100

    Include cost profiles which are approximations of reference, but tweaked to provide further separation

--- app/dto/cost_profile.py
+++ app/dto/cost_profile.py
@@ -6,7 +6,11 @@ class CostProfile(NamedTuple):
   beta: float
   gamma: float
 
-COST_PRESETS = collections.namedtuple('cost_presets', ['COAL', 'OIL'])(
-    COAL=CostProfile(alpha=510.0, beta=7.2, gamma=0.00142),
-    OIL=CostProfile(alpha=310.0, beta=7.85, gamma=0.00194),
+COST_PRESETS = collections.namedtuple('cost_presets', ['COAL', 'OIL', 'GAS'])(
+    # COAL=CostProfile(alpha=510.0, beta=7.2, gamma=0.00142),
+    # OIL=CostProfile(alpha=310.0, beta=7.85, gamma=0.00194),
+    # OIL_ALTERNATE(alpha=78.0, beta=7.97, gamma=0.00482),
+    COAL=CostProfile(alpha=300.0, beta=5, gamma=0.00100),
+    OIL=CostProfile(alpha=500.0, beta=8, gamma=0.00194),
+    GAS=CostProfile(alpha=700.0, beta=10, gamma=0.00482),
 )
