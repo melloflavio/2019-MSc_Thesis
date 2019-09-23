df46a238b705f765c08b3dc964151f5a1be97942
Flavio Mello <flaviorafmello@gmail.com>
2019-09-23 16:06:57 +0100

    Bugfix model loading in action_composition model tester

--- app/learning/__init__.py
+++ app/learning/__init__.py
@@ -6,3 +6,4 @@ from .critic import Critic
 from .model_trainer import ModelTrainer
 from .model_tester import ModelTester
 from .model_adapter import ModelAdapter
+from .model_tester_action_composition import ModelTesterActionComposition
